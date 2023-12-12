f160b843b145734e1518793ec7c5ca9e9d32c6a0
Wang Ziyuan <756881257@qq.com>
2023-12-12 20:19:02 +0100

    transformer halfway 2

--- NLP/transformer.py
+++ NLP/transformer.py
@@ -1,180 +1,104 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as f
 import numpy as np
-from torchsummary import summary
 
-# after embedding: batch_size, source_len, d_model
-
-d_model = 512 # length of embedding
-d_k = 8 # length of Q and K
-d_v = 8 # length of V
-d_ff = 2048 # length of feed forward network
-heads = 6 # number of heads
-batch_size = 100
-src_len = 5 # the length of input to encoder
-src_voc_len = 10 # source vovabulary length
-tgt_voc_len = 10 # target vocabulary length
-tgt_len = 5 # the length of input to decoder
-n_layer = 6
-
-def PositionalEncoding_table(n_pos, d_model):
-    def calc_angle(posi, i):
-        if i % 2 != 0:
-            i = i - 1
-        return posi / np.power(10000, 2*i / d_model)
-    
-    def get_posi_vec(posi):
-        return [calc_angle(posi, i) for i in range(d_model)] # a list with d_model length
-    
-    sinusoid_table = np.array([get_posi_vec(i) for i in range(n_pos)])
-    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2]) # even index
-    sinusoid_table[:, 1::2] = np.sin(sinusoid_table[:, 1::2]) # odd index
-    return torch.FloatTensor(sinusoid_table)
-
-def get_padding(q, k):
-    # the shape of q: B, len_q
-    # the shape of k: B, len_k
-    batch_size, len_q = q.size()
-    batch_size, len_k = k.size() 
-    # if there are trivial words, set it true, later we will mask it
-    padding_tensor = k.eq(0).unsqueeze(1) # B, 1, len_k
-    padding_tensor = padding_tensor.expand(batch_size, len_q, len_k)
-    return padding_tensor # B, len_q, len_k
-
-def get_subsequent_padding(seq):
-    seq_shape = [seq.size(0), seq.size(1), seq.size(1)]
-    subsequent_mask = np.triu(np.ones(seq_shape), k = 1)
-    return torch.from_numpy(subsequent_mask).byte() # uint8 type, upper limit is 255
-    
-
-class ScaledDot_Product(nn.Module):
-    def __init__(self):
+class LayerNorm(nn.Module):
+    def __init__(self, x, eps = 1e-6):
         super().__init__()
-    
-    def forward(self, q_s:torch.TensorType, k_s:torch.TensorType, v_s:torch.TensorType, mask):
-        dot_product = torch.matmul(q_s, k_s.transpose(-1, -2)) / np.sqrt(d_k)
-        dot_product.masked_fill_(mask, 1e-9)
-        dot_product = nn.Softmax(dim=-1)(dot_product)
-        result = torch.matmul(dot_product, v_s)
-        # (B, H, S, D(K))
-        return result
-
-class Multihead_Attention(nn.Module):
-    def __init__(self) -> None:
-        # input: batch_size(B), source_len(S), d_model(D)
-        # Q, k, V
-        super(Multihead_Attention, self).__init__()
-        self.Q = nn.Linear(d_model, d_k * heads) # batch, source
-        self.K = nn.Linear(d_model, d_k * heads)
-        self.V = nn.Linear(d_model, d_v * heads)
-        self.linear = nn.Linear(heads * d_k, d_model)
-        self.layer_norm = nn.LayerNorm(d_model)
-    
-    # (B, S, D(model)) -project--> (B, S, D(k) * H) -split--> (B, S, H, D(K)) -transpose--> (B, H, S, D(K))
-    # -Flatten--> (B, S, H * D(K)) -project--> (B, S, D(model))
-    def forward(self, q_input, k_input, v_input, mask):
-        residue, batch_size = q_input, q_input.size(0)
-        q_s = self.Q(q_input).view(batch_size, -1, heads, d_k).transpose(1, 2)
-        k_s = self.K(k_input).view(batch_size, -1, heads, d_k).transpose(1, 2)
-        v_s = self.V(v_input).view(batch_size, -1, heads, d_v).transpose(1, 2)
-        mask = mask.unsqueeze(1).repeat(1, heads, 1, 1) # B, len_q, len_k -> B, H, len_q, len_k
-        result = ScaledDot_Product()(q_s, k_s, v_s, mask)
-        result = result.transpose(1, 2).reshape(batch_size, -1, heads * d_k)
-        # print(result.is_contiguous())
-        result = self.linear(result)
-        return self.layer_norm(residue + result) 
-
-class PointWise_FeedForward(nn.Module):
-    # (B, S, D(model))
-    def __init__(self):
-        super(PointWise_FeedForward, self).__init__()
-        self.mlp1 = nn.Linear(d_model, d_ff)
-        self.relu = nn.ReLU()
-        self.mlp2 = nn.Linear(d_ff, d_model)
+        # nn.Parameter will add tensors into the module.parameters()
+        # beta * (x - miu) + gamma, for each feature
+        self.beta = nn.Parameter(torch.zeros(x))
+        self.gamma = nn.Parameter(torch.ones(x))
+        self.eps = eps
     
     def forward(self, x):
-        residual = x
-        x = self.mlp1(x)
-        x = self.relu(x)
-        x = self.mlp2(x)
-        return nn.LayerNorm(d_model)(x + residual)
-    
-class Encoder_Layer(nn.Module):
-    def __init__(self):
+        """
+        x: B, L (number of Q), F (feature)
+        """
+        x_mean = torch.mean(x, dim=-1, keepdim=True) # B, L
+        x_std = torch.std(x, dim=-1, keepdim=True) # B, L
+        return self.ones_parameter * (x - x_mean) / (x_std + self.eps)
+
+class ScaledDotProduct(nn.Module):
+    def __init__(self, ):
         super().__init__()
-        self.multihead = Multihead_Attention()
-        self.feedforward = PointWise_FeedForward()
-    
-    def forward(self, x, self_attn_mask):
-        x = self.multihead(x, x, x, self_attn_mask)
-        x = self.feedforward(x)
-        return x
-    
-class Decoder_Layer(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.dec_self_att = Multihead_Attention()
-        self.dec_enc_att = Multihead_Attention()
-        self.feedforward = PointWise_FeedForward()
-    
-    def forward(self, dec_input, enc_output, dec_self_mask, dec_enc_mask):
-        dec_output = self.dec_self_att(dec_input, dec_input, dec_input, dec_self_mask)
-        # print(dec_output.shape)
-        dec_output = self.dec_enc_att(dec_output, enc_output, enc_output, dec_enc_mask)
-        dec_output = self.feedforward(dec_output)
-        return dec_output
-    
-class Encoder(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.voc_embedding = nn.Embedding(src_voc_len, d_model)
-        self.positional_encode = nn.Embedding.from_pretrained(PositionalEncoding_table(src_len + 1, d_model), freeze=True)
-        self.encoder_layer = nn.ModuleList([Encoder_Layer() for _ in  range(n_layer)])
-    
-    def forward(self, encode_input):
-        sequence_number = [i for i in range(1, src_len)]
-        sequence_number.append(0) # zero is for mask
-        self_attn_mask = get_padding(encode_input, encode_input)
-        encode_output = self.voc_embedding(encode_input) + self.positional_encode(torch.LongTensor(sequence_number))
-        for enc_layer in self.encoder_layer:
-            encode_output = enc_layer(encode_output, self_attn_mask)
-        return encode_output
-    
-class Decoder(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.voc_embedding = nn.Embedding(tgt_voc_len, d_model)
-        self.positional_encode = nn.Embedding.from_pretrained(PositionalEncoding_table(tgt_len + 1, d_model), freeze=True)
-        self.layers = nn.ModuleList([Decoder_Layer() for _ in range(n_layer)])
-    
-    def forward(self, dec_input, enc_output):
-        sequence = [tgt_len]
-        sequence.extend([i for i in range(1, tgt_len)])
-        dec_outputs = self.voc_embedding(dec_input) + self.positional_encode(torch.LongTensor(sequence))
-        self_dec_mask = get_subsequent_padding(dec_input)
-        self_dec_pad_mask = get_padding(dec_input, dec_input)
-        self_total_mask = torch.gt(self_dec_mask + self_dec_pad_mask, 0)
-        for layer in self.layers:
-            dec_outputs = layer(dec_outputs, enc_output, self_total_mask, self_dec_pad_mask)
-        return dec_outputs
-    
-class Transformer(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.projection = nn.Linear(d_model, tgt_voc_len)
-    
-    def forward(self, enc_input, dec_input):
-        enc_output = self.encoder(enc_input)
-        dec_output = self.decoder(dec_input, enc_output)
-        dec_output = self.projection(dec_output)
-        return dec_output
+        
+    def forward(self, q, k, v):
+        d_k = q.size(-1)
+        # B, H_num, L, H_dim
+        scores = torch.matmul(q, k.transpose(-1, -2)) / np.sqrt(d_k)
+        scores = f.softmax(scores, dim=-1)
+        # B, H_num, L, d_v
+        return torch.matmul(scores, v)
         
 
-if __name__ == '__main__':
-    transformer = Transformer()
-    tensor = torch.rand(1, 5).type(torch.LongTensor)
-    print(transformer(tensor, tensor).shape)
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads) -> None:
+        super().__init__()
+        head_dim = d_model // num_heads
+        assert head_dim * num_heads == d_model
+        self.head_dim = head_dim
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.query = nn.Linear(self.d_model, self.d_model)
+        self.key = nn.Linear(self.d_model, self.d_model)
+        self.value = nn.Linear(self.d_model, self.d_model)
+        self.attention_function = ScaledDotProduct()
+        self.out_linear = nn.Linear(d_model, d_model)
+        
+    def forward(self, q, k, v):
+        batsz = q.size(0)
+        # B, SouceLength, d_model -> B, SouceLength, H_num, H_dim -> B, H_num, SouceLength, H_dim
+        query = self.query(q).view(batsz, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        key = self.key(k).view(batsz, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        value = self.value(v).view(batsz, -1, self.num_heads, self.head_dim).transpose(1, 2)
+        x = self.attention_function(query, key, value)
+        x = x.transpose(1, 2).contiguous().view(batsz, -1, self.d_model)
+        x = self.out_linear(x)
+        return x
     
-    # print(PositionalEncoding_table(src_len, d_model).shape)
+class PointWiseFeedForward(nn.Module):
+    def __init__(self, d_model = 512, dim_feedforward = 2048):
+        super().__init__()
+        self.sequential = nn.Sequential(
+            nn.Linear(d_model, dim_feedforward),
+            nn.ReLU(),
+            nn.Linear(dim_feedforward, d_model)
+        )
+        self.layernorm = LayerNorm(d_model)
+    
+    def forward(self, x):
+        src = self.sequential(x)
+        return self.layernorm(x + src)
+    
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model = 512, num_heads = 8):
+        super().__init__()
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+        self.feedforward = PointWiseFeedForward(d_model)
+        self.layernorm = LayerNorm(d_model)
+        
+    def forward(self, x):
+        src = self.self_attn(x, x, x)
+        src = self.layernorm(x + src)
+        src = self.feedforward(src)
+        return src
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self, encoder_layer = TransformerEncoderLayer(), num_layers = 6):
+        super().__init__()
+        self.encoder_layer = nn.ModuleList([encoder_layer for _ in range(num_layers)])
+    
+    def forward(self, x):
+        for layer in self.encoder_layer:
+            x = layer(x)
+        return x
+
+if __name__ == '__main__':
+    # layernorm = LayerNorm()
+    module = MultiHeadAttention(512, 8)
+    # _layernorm = nn.LayerNorm(512)
+    x = torch.rand([32, 10, 512])
+    # print(_layernorm(x))
+    print(module(x, x, x).shape)
